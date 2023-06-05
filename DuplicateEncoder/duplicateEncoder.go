faba75207aad1e6b9b05da6f97310704364821fa
tohani <tohani@test.com>
2023-06-06 01:30:20 +0300

    duplicate encoder added

---
+++ DuplicateEncoder/duplicateEncoder.go
@@ -0,0 +1,53 @@
+package main
+
+import "strings"
+
+// DuplicateEncode takes a string and returns a new string where
+func DuplicateEncode(word string) string {
+	// Enter solution here
+
+	newWord := ""
+
+	word = strings.ToLower(word)
+
+	for _, v := range word {
+		// if v is in word more than once, return ")"
+		if strings.Count(word, string(v)) > 1 {
+			newWord += ")"
+		} else {
+			// otherwise, return "("
+			newWord += "("
+		}
+
+	}
+
+	return newWord
+}
+
+// This solution is the same as above, but uses a for loop instead of strings.Count
+
+// func DuplicateEncode(word string) string {
+// 	// Enter solution here
+
+// 	newWord := ""
+
+// 	word = strings.ToLower(word)
+
+// 	for _, v := range word {
+// 		count := 0
+// 		// if v is in word more than once, return ")"
+// 		// count the number of times v is in word
+// 		for _, v2 := range word {
+// 			if v == v2 {
+// 				count++
+// 			}
+// 		}
+// 		if count > 1 {
+// 			newWord += ")"
+// 		} else {
+// 			// otherwise, return "("
+// 			newWord += "("
+// 		}
+// 	}
+// 	return newWord
+// }
---
+++ DuplicateEncoder/duplicateEncoder_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestDuplicateEncode(t *testing.T) {
+	type args struct {
+		word string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"din", args{"din"}, "((("},
+		{"recede", args{"recede"}, "()()()"},
+		{"Success", args{"Success"}, ")())())"},
+		{"(( @", args{"(( @"}, "))(("},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DuplicateEncode(tt.args.word); got != tt.want {
+				t.Errorf("DuplicateEncode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
