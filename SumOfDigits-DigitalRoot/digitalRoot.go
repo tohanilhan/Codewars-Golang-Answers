f4bc57a0e949ec516d943f94e5ed4247465eaebf
tohani <tohani@test.com>
2023-06-06 01:13:02 +0300

    digitalRoot added

---
+++ SumOfDigits-DigitalRoot/digitalRoot.go
@@ -0,0 +1,22 @@
+package main
+
+func DigitalRoot(n int) int {
+	// ...
+
+	// if n bigger than or equal to 10 , sum the digits
+	for n >= 10 {
+		sum := 0
+		// sum the digits
+		for n > 0 {
+			// add the last digit to the sum
+			sum += n % 10
+			// remove the last digit
+			n /= 10
+		}
+		// assign the sum to n
+		n = sum
+
+		// if n is smaller than 10, return n
+	}
+	return n
+}
---
+++ SumOfDigits-DigitalRoot/digitalRoot_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestDigitalRoot(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"16", args{16}, 7},
+		{"195", args{195}, 6},
+		{"992", args{992}, 2},
+		{"167346", args{167346}, 9},
+		{"0", args{0}, 0},
+		{"942", args{942}, 6},
+		{"132189", args{132189}, 6},
+		{"493193", args{493193}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DigitalRoot(tt.args.n); got != tt.want {
+				t.Errorf("DigitalRoot() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
