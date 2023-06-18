eb73ed5c17d8dfb452b2e0e0b1cb6800cfdc7fee
Tohan  İLHAN <atahantohan@gmail.com>
2023-06-18 22:14:31 +0300

    refactored

--- SumOfIntervals/sumOfIntervals.go
+++ SumOfIntervals/sumOfIntervals.go
@@ -5,55 +5,24 @@ import "sort"
 func SumOfIntervals(intervals [][2]int) int {
 	// your code here
 
-	lenght := 0
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
 
-	mergedIntervals := merge(intervals)
-
-	for _, interval := range mergedIntervals {
-		lenght += interval[1] - interval[0]
-	}
-
-	return lenght
-}
-
-type intervalsArray [][2]int
-
-func (intA intervalsArray) Len() int {
-	return len(intA)
-}
-
-func (intA intervalsArray) Swap(i, j int) {
-	intA[i], intA[j] = intA[j], intA[i]
-}
-
-func (intA intervalsArray) Less(i, j int) bool {
-	return intA[i][0] < intA[j][0]
-}
-
-func merge(intervals [][2]int) [][2]int {
-
-	intA := intervalsArray(intervals)
-
-	sort.Sort(intA)
-
-	intervalsSorted := [][2]int(intA)
-	//fmt.Println(intervalsSorted)
-
-	var output [][2]int
-	currentIntervalStart := intervalsSorted[0][0]
-	currentIntervalEnd := intervalsSorted[0][1]
-	for j := 1; j < len(intervalsSorted); j++ {
-		if currentIntervalEnd >= intervalsSorted[j][0] {
-			if intervalsSorted[j][1] > currentIntervalEnd {
-				currentIntervalEnd = intervalsSorted[j][1]
+	length := 0
+	lowest := intervals[0][0]
+	for _, v := range intervals {
+		if v[1] >= lowest {
+			length += v[1]
+			if v[0] > lowest {
+				length -= v[0]
+			} else {
+				length -= lowest
 			}
-		} else {
-			output = append(output, [2]int{currentIntervalStart, currentIntervalEnd})
-			currentIntervalStart = intervalsSorted[j][0]
-			currentIntervalEnd = intervalsSorted[j][1]
+			lowest = v[1]
 		}
 	}
-	output = append(output, [2]int{currentIntervalStart, currentIntervalEnd})
-	return output
+
+	return length
 
 }
